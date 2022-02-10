b322b367f283c1e292083d30f1e9e29e4c0a72be
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-10 18:19:20 +0300

    template works

--- first_project/pages/urls.py
+++ first_project/pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HomeView, name='home')
+    path('', views.HomeView, name='home'),
+    path('hello/',views.HomePageView.as_view(), name='class_home'),
 ]
