8741a597c627a2a38acd7185ac2b6bede1e810be
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-10 18:22:52 +0300

    about page added

--- first_project/pages/urls.py
+++ first_project/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView, name='home'),
     path('hello/',views.HomePageView.as_view(), name='class_home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
