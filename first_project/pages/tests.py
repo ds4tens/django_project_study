6725d75d05aa29faf1606079e44cc00acf34a014
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-10 18:42:11 +0300

    added Simple Tests

--- first_project/pages/tests.py
+++ first_project/pages/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SimpleTests(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/hello/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_aboutpage_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
--- first_project/pages/urls.py
+++ first_project/pages/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView, name='home'),
-    path('hello/',views.HomePageView.as_view(), name='class_home'),
+    path('hello/',views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutPageView.as_view(), name='about'),
 ]
